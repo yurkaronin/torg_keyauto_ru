05a12ea581dfa56708b027b6438158ac704b1432
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2024-11-26 18:57:08 +0300

    Исправляю ошибку в компоненте JS mobile-nav-open

--- js/components/mobile-nav-open.js
+++ js/components/mobile-nav-open.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Обработка открытия и закрытия мобильного меню
   var mobileButtonMenu = document.querySelector('.js-menu-button-mobile');
-  mobileButtonMenu.addEventListener('click', function () {
-    document.body.classList.toggle('mobile-nav-open');
-  });
+  if (mobileButtonMenu) {
+    mobileButtonMenu.addEventListener('click', function () {
+      document.body.classList.toggle('mobile-nav-open');
+    });
+
+  }
 
 });
