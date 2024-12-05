dc9013aee20e687a84531aabe2a1553334733bf2
Yuriy Savchenko <yurkaronin@gmail.com>
2024-12-05 08:19:12 +0300

    Детальная карточка лота АДАПТИВ

--- js/components/lot-card-slider.js
+++ js/components/lot-card-slider.js
@@ -4,17 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var swiper = new Swiper(".js-lot-card-slider .swiper", {
       loop: true,
-      slidesPerView: 1.5,
+      // slidesPerView: 1.5,
       spaceBetween: 1,
-      centeredSlides: true,я
+      centeredSlides: true,
       navigation: {
         nextEl: ".js-lot-card-slider .swiper-button-next",
         prevEl: ".js-lot-card-slider .swiper-button-prev",
       },
       breakpoints: {
-        // 1600: {
-        //   slidesPerView: 10,
-        // },
+        550: {
+          slidesPerView: 1.5,
+        },
+        320: {
+          slidesPerView: 1,
+        },
 
       }
 
